7b7135258582fd687e0b0b937a637b4370352fa6
skygun <skygun88@naver.com>
2020-08-12 03:37:44 +0900

    Template id transfer implemented

--- test_app/views.py
+++ test_app/views.py
@@ -27,18 +27,20 @@ def api_test(request, content):
     if request.method == 'POST':
         print("Test - POST")
         ''' 1. Get Image file from content as temp_img_ + date + .jpg '''
+        templateId = int(content)
         image_file = request.FILES.get('uploaded_file')
+        print("Template_ID: " + str(templateId) + ", File: " + str(request.FILES.get('uploaded_file')))
         filename = 'temp/temp_img_' + str(datetime.datetime.now()) + '.jpg'
         path = default_storage.save(filename, ContentFile(image_file.read()))
         tmp_file = os.path.join(settings.MEDIA_ROOT, path)
-        print("Save finish")
+        print("Requested image Saved")
         ''' 2. Image Processing by using GAN model -- YeJi's part  '''
-        new_path = imgProcessing(tmp_file)
+        new_path = imgProcessing(tmp_file, templateId)
         ''' 3. Send Json Response message containing Processed image (ID picture) '''
         with open(new_path, mode='rb') as file:
             img = file.read()
         data = {'img': base64.encodebytes(img).decode("utf-8")}
-        print(data['img'][:30])
+        #print(data['img'][:30])
         deleteTemp(tmp_file, new_path)
         return JsonResponse(data)
     elif request.method == 'GET':
@@ -46,11 +48,13 @@ def api_test(request, content):
     return HttpResponse("<http><body>hihihi</body></http>")
 
 ''' Function to process the face image to ID Picture '''
-def imgProcessing(path):
+def imgProcessing(path, templateId):
     filename = path.split('/')[-1]
+    print(path)
     img = Image.open(path)
     im_flip = filpImage(img)
     new_filename = path.replace(filename, 'temp_processed_') + str(datetime.datetime.now()) + '.jpg'
+    print(new_filename)
     im_flip.save(new_filename, quality=100)
     return new_filename
 
@@ -61,6 +65,6 @@ def filpImage(img):
 ''' Delete the temporary file '''
 def deleteTemp(path1, path2):
     if os.path.isfile(path1):
-        os.remove(path2)
-    if os.path.isfile(path1):
+        os.remove(path1)
+    if os.path.isfile(path2):
         os.remove(path2)
